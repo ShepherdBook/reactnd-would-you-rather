16b83a946814ebe47e6a7bfb58b594a83efe45d9
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2019-03-17 13:36:35 -0400

    feat: Show vote count and percentage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,12 +3,23 @@ import { connect } from 'react-redux'
 import Question from './Question'
 
 class Dashboard extends Component {
+
+  // local state to track showAnsweredQuestions toggle
+
   render() {
     return (
       <div>
-        <h3>Questions</h3>
+        <h3>Unanswered Questions</h3>
         <ul>
-          {this.props.questionIds.map((id) => (
+          {this.props.unansweredQuestionIds.map((id) => (
+            <li key={id}>
+              <Question id={id} />
+            </li>
+          ))}
+        </ul>
+        <h3>Answered Questions</h3>
+        <ul>
+          {this.props.answeredQuestionIds.map((id) => (
             <li key={id}>
               <Question id={id} />
             </li>
@@ -20,9 +31,12 @@ class Dashboard extends Component {
 }
 
 function mapStateToProps({ questions }) {
+
   return {
-    questionIds: Object.keys(questions)
-      .sort((a, b) => questions[b].timestamp - questions[a].timestamp)
+    answeredQuestionIds: Object.keys(questions)
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
+    unansweredQuestionIds: Object.keys(questions)
+      .sort((a, b) => questions[b].timestamp - questions[a].timestamp),
   }
 }
 
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { formatQuestion, formatDate } from '../utils/helpers'
+import { formatDate } from '../utils/helpers'
 
 class Question extends Component {
   render() {
-    const { question, author } = this.props
+    const { question, author, users, userCount } = this.props
+
+    const optionOneVoteCount = question.optionOne.votes.length
+    const optionTwoVoteCount = question.optionTwo.votes.length
+
+    const optionOnePercentage = (optionOneVoteCount === 0 
+      ? 0 
+      : (optionOneVoteCount / userCount)).toFixed(2) * 100
+
+    const optionTwoPercentage = (optionTwoVoteCount === 0 
+      ? 0 
+      : (optionTwoVoteCount / userCount)).toFixed(2) * 100
 
     return (
       <div>
         <h3>Would you rather...</h3>
-        <p>{question.optionOne.text} OR {question.optionTwo.text}?</p>
+        <p>{question.optionOne.text} {optionOnePercentage}% chose this ({optionOneVoteCount})</p>
+        <p>{question.optionTwo.text} {optionTwoPercentage}% chose this ({optionTwoVoteCount})</p>
         <p>Asked by {author.name} on {formatDate(question.timestamp)}</p>
+
       </div>
     )
   }
@@ -20,8 +33,11 @@ function mapStateToProps({ activeUser, users, questions }, { id }) {
   const question = questions[id]
   const author = users[question.author]
 
+  const userCount = Object.keys(users).length
+
   return {
     activeUser,
+    userCount,
     question,
     author
   }
